f16c0ddfa4d4117a89be5ee27c5facc099122165
nerosgonul <soren.gonul@gmail.com>
2023-08-14 20:39:53 +0300

    app js geliştirmeleri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+
+const EmmployeeProfiles = () => {
+
+  const [employeeProfiles, setEmployeeProfiles] = useState([]);
+
+  const fetchEmployeeProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/employee/list").then(resp => {
+      setEmployeeProfiles(resp.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchEmployeeProfiles();
+  }, []);
+
+  return employeeProfiles.map((employee, index) => {
+      return (
+      <div key={index}>
+        <h1>{employee.name}</h1>
+        <h2>{employee.email}</h2>
+        <h2>{employee.dob}</h2>
+        <h3>{employee.age}</h3>
+      </div>
+    )
+  })
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EmmployeeProfiles />
     </div>
   );
 }
